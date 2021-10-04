81eb1c22aaed255ef367f853b82a8949adab070d
Arihant Jain <jain.ari2000@gmail.com>
2021-10-04 18:56:14 +0530

    added basic SCSS and dark mode config

--- iiitdmj/src/index.js
+++ iiitdmj/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
-
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+import ThemeProvider from "./styles/Theme/ThemeProvider";
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <ThemeProvider>
+      <App />
+      </ThemeProvider>
+    </Provider>,
   document.getElementById('root')
 );
+serviceWorker.unregister();
