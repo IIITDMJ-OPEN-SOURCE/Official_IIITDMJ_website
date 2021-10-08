0b67acfec161684e048a18abfb839cb899e201fe
Arihant Jain <jain.ari2000@gmail.com>
2021-10-08 11:25:50 +0530

    removed redux and fixed package-lock.json

--- iiitdmj/src/index.js
+++ iiitdmj/src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import ThemeProvider from "./styles/Theme/ThemeProvider";
 ReactDOM.render(
-    <Provider store={store}>
-      <ThemeProvider>
+    <ThemeProvider>
       <App />
-      </ThemeProvider>
-    </Provider>,
+     </ThemeProvider>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
